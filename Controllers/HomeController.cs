a1251428ec7b37ac3c83e6cc37c8b4b41950a284
michaelmunkwitzbennett <michaelmunkwitzbennett@gmail.com>
2020-02-03 17:03:16 -0600

    Before Assignment 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Modas.Models;
 
 namespace Modas.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly int PageSize = 10;
         private IEventRepository repository;
         public HomeController(IEventRepository repo)
         {
             repository = repo;
         }
-        public ViewResult Index() => View(repository.Events);
+        public ViewResult Index(int page = 1) => View(
+            repository.Events.Include(e => e.Location)
+                .OrderBy(e => e.TimeStamp)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Modas.Models;
 
@@ -12,11 +14,19 @@ namespace Modas
 {
     public class Startup
     {
+        // this exposes the connection string in appsettings.json
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IEventRepository, FakeEventRepository>();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:Modas:ConnectionString"]));
+            services.AddTransient<IEventRepository, EFEventRepository>();
             services.AddMvc();
         }
 
@@ -28,8 +38,19 @@ namespace Modas
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvcWithDefaultRoute();
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "pagination",
+                    template: "Page{page}",
+                    defaults: new { Controller = "Home", action = "Index" });
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
             app.UseStaticFiles();
+            SeedData.EnsurePopulated(app);
         }
     }
 }
